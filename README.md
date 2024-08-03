d9fbdc77c813610a6543eeb5bb53dcc43cf101bf
christ47 <imani.c.christie@gmail.com>
2024-08-03 13:09:42 +0100

    read me updated

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# playwright-playground
-Playground for Playwright 
+# Playwright Playground
+
+This repo is an example of different testing methods available through playwright. 
+
+# Prequisuites
+Node
+
+Ensure dependancies are installed.
+
+## Usage
+
+To run all suites run the following command in the root of the project:
+$ yarn test
--- tests/vanillaPlaywright.spec.js
+++ tests/vanillaPlaywright.spec.js
@@ -29,13 +29,4 @@ test.describe("Basic UI automation test for an eccomerce", () => {
     await expect(page).toHaveTitle(/Playwright/);
   });
 
-  test("get started link", async ({ page }) => {
-    await page.goto("https://playwright.dev/");
-
-    // Click the get started link.
-    await page.getByRole("link", { name: "Get started" }).click();
-
-    // Expects the URL to contain intro.
-    await expect(page).toHaveURL(/.*intro/);
   });
-});
